93caa0e8fd6b0e17f4ac5f3b9c8181c1e1c3d6d0
qi xu <qi.xu@spectrum-safety.com>
2025-07-21 22:06:21 +0200

    change the server port to 3030

--- app/src/serverConfig.js
+++ app/src/serverConfig.js
@@ -1,5 +1,5 @@
 const SERVER_URL =
   import.meta.env.VITE_SERVER_URL ||
-  `${window.location.protocol}//${window.location.hostname}:3000`;
+  `${window.location.protocol}//${window.location.hostname}:3030`;
 
 export default SERVER_URL;
