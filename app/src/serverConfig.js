a9774220566d5b602203eae5adfa0d169eb32df3
qi xu <qi.xu@spectrum-safety.com>
2025-07-22 07:10:34 +0200

    Change the server API address

--- app/src/serverConfig.js
+++ app/src/serverConfig.js
@@ -1,5 +1,6 @@
 const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL ||
-  `${window.location.protocol}//${window.location.hostname}:3030`;
+  import.meta.env.MODE === 'development'
+    ? `${window.location.protocol}//${window.location.hostname}:3030`
+    : 'https://guandan-score-api.onrender.com';
 
 export default SERVER_URL;
